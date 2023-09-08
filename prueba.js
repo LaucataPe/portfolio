9519bcb07145ed5d90563387381cf36343ac3cf3
Laura Perez <laucatape@gmail.com>
2023-09-07 22:57:58 -0500

    Improve the MyName part

---
+++ prueba.js
@@ -0,0 +1,46 @@
+const iva_bimestral = [ 
+    { month: '09', day: '07', nit: 1 },
+    { month: '09', day: '08', nit: 2 },
+    { month: '09', day: '11', nit: 3 },
+    { month: '09', day: '12', nit: 4 },
+    { month: '09', day: '13', nit: 5 },
+    { month: '09', day: '14', nit: 6 },
+    { month: '09', day: '15', nit: 7 },
+    { month: '09', day: '18', nit: 8 },
+    { month: '09', day: '19', nit: 9 },
+    { month: '09', day: '20', nit: 0 },
+    { month: '11', day: '08', nit: 1 },
+    { month: '11', day: '09', nit: 2 },
+    { month: '11', day: '10', nit: 3 },
+    { month: '11', day: '14', nit: 4 },
+    { month: '11', day: '15', nit: 5 },
+    { month: '11', day: '16', nit: 6 },
+    { month: '11', day: '17', nit: 7 },
+    { month: '11', day: '20', nit: 8 },
+    { month: '11', day: '21', nit: 9 },
+    { month: '11', day: '22', nit: 0 }
+]
+
+const iva_cuatrimestral = [
+    { month: '09', day: '07', nit: 1 },
+    { month: '09', day: '08', nit: 2 },
+    { month: '09', day: '11', nit: 3 },
+    { month: '09', day: '12', nit: 4 },
+    { month: '09', day: '13', nit: 5 },
+    { month: '09', day: '14', nit: 6 },
+    { month: '09', day: '15', nit: 7 },
+    { month: '09', day: '18', nit: 8 },
+    { month: '09', day: '19', nit: 9 },
+    { month: '09', day: '20', nit: 0 }
+  ]
+
+const ica = [
+    { month: '10', day: '20'},
+    { month: '12', day: '15'},
+]
+
+const rte_ica = [
+    { month: '09', day: '15'},
+    { month: '11', day: '17'},
+]
+
--- src/components/MyName.tsx
+++ src/components/MyName.tsx
@@ -5,12 +5,17 @@ function MyName() {
     return (
       <>
       <div className="h-[100hv] flex items-center justify-center bg-blue-300">
-        <h1 className="text-3xl font-bold">I'm Laura. Let's make your developments come true!</h1>
+        <div className='w-[60%] mx-[10%]'>
+          <h1 className="text-6xl font-bold">I'm Laura. Let's make your developments come true!</h1>
+          <div className='flex my-5 gap-3'>
+            <Button icon="pi pi-github" outlined rounded className='hover:bg-red-300'/>
+            <Button icon="pi pi-linkedin" outlined rounded/>
+            <Button icon="pi pi-book" outlined rounded/>
+          </div>
+          <button className='bg-blue-500 p-5 rounded-full text-white font-bold text-xl
+          hover:bg-white hover:text-blue-500 transition-colors'>Empezar ahora</button>
+        </div>
         <img src={coco} alt="Me foto" />
-        <Button icon="pi pi-github" outlined rounded className='hover:bg-red-300'/>
-        <Button icon="pi pi-linkedin" outlined rounded/>
-        <Button icon="pi pi-book" outlined rounded/>
-        <Button label="Let's work togheter" rounded/>
       </div>
       </>
     )
